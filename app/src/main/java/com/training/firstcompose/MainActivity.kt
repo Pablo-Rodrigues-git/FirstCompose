c7406914d995ff69d8d2f4f75ba1d7f8941ddc60
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-05-13 02:09:15 -0300

    Initial commit for compose

---
+++ app/src/main/java/com/training/firstcompose/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.training.firstcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.training.firstcompose.ui.theme.FirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var selecionada =  remember { mutableStateOf(false) }
+    val imagem = if (selecionada.value) {
+        painterResource(id = R.drawable.ic_check_filled)
+    } else{
+        painterResource(id = R.drawable.ic_check)
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.frame__5_),
+            contentDescription = "Produto",
+            modifier = Modifier
+                .size(56.dp)
+                .padding(end = 8.dp))
+
+        Column() {
+            Text(text = "Título")
+            Text(text = "R$ 70,00")
+        }
+        
+        IconButton(onClick = {
+            selecionada.value = !selecionada.value
+        }) {
+            Image(
+                painter = imagem,
+                contentDescription = "Botão de Seleção",
+                modifier = Modifier.size(32.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FirstComposeTheme {
+        Greeting("Android")
+    }
+}
